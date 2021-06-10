9f045de11f3e8556a588956ddf3d78a05b9e5250
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-10 21:42:34 +0200

    added conditionals script

--- scripts/Strings.js
+++ scripts/Strings.js
@@ -1,5 +1,16 @@
+let song = 'Fight the Youth';
+song = `Fight the Youth`;
+
+
 let score = 9;
 let highestScore = 10;
 let output = `I like the song "${ song }". I gave it a score of ${ score/highestScore * 100 }%.`;
 
-document.getElementById("strings").innerHTML = "test " + scoreout;
+document.getElementById("strings").innerHTML = "test " + output;
+
+
+function basics() {
+    console.log("test")
+}
+
+basics();
