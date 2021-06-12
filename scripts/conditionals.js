7c7eed48525d9f5bea191f60d31d106e84200c10
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-12 18:09:45 +0200

    clearing up

--- scripts/conditionals.js
+++ scripts/conditionals.js
@@ -1,47 +1,38 @@
- const select = document.querySelector('select');
-const para = document.querySelector('p');
+
+const select = document.querySelector('select');
+// const para = document.querySelector('p');
+// beides ist möglich
+const para = document.getElementById('para');
+const html = document.querySelector('html');
+
 
 select.addEventListener('change', setWeather);
 
 function setWeather() {
   const choice = select.value;
-
-/*   if (choice === 'sunny') {
-    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
-  } else if (choice === 'rainy') {
-    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
-  } else if (choice === 'snowing') {
-    para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
-  } else if (choice === 'overcast') {
-    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
-  } else {
-    para.textContent = '';
+  switch (choice) {
+    case 'sunny':
+      para.textContent = 'It is nice and sunny outside today';
+      break;
+    case 'rainy':
+      para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+      break;
+    case 'snowing':
+      para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+      break;
+    case 'overcast':
+      para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+      break;
+    default:
+      para.textContent = '';
   }
-*/
+}
 
-"Nesting if ... else"
- let temperature = 90;
-/*  if (choice === 'sunny') {
-    if (temperature < 86) {
-      para.textContent = 'It is ' + temperature + ' degrees outside — nice and sunny. Let\'s go out to the beach, or the park, and get an ice cream.';
-    } else if (temperature >= 86) {
-      para.textContent = 'It is ' + temperature + ' degrees outside — REALLY HOT! If you want to go outside, make sure to put some sunscreen on.';
-    }
-  }
- */
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
 
-"Logical operators: AND, OR and NOT"
-
-/*   if (choice === 'sunny' && temperature <86) {
-    para.textContent = 'It is ' + temperature + 'degrees outside.'; 
-  } else if (choice === 'sunny' && temperature >= 86) {
-    para.textContent = 'It is ' + temperature + ' degrees outside - Realy hot!'
-  }
-} *
-
-"another test"
-if (iceCreamVanOutside || houseStatus === 'on fire') {
-  console.log('You should leave the house quickly.');
-} else {
-  console.log('Probably should just stay in then.');
+select.onchange = function () {
+  (select.value === 'black') ? update('#333333', 'white') : update('white', '#333333');
 }
