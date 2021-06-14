588850c59ff30536cbbb1580c0653cd04a8273dc
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-14 22:32:06 +0200

    show the sign

--- scripts/conditionals.js
+++ scripts/conditionals.js
@@ -1,7 +1,21 @@
-let question = prompt("What is the 'official' name of JavaScript");
+/* let question = prompt("What is the 'official' name of JavaScript");
 
 if (question === "ECMAScript") {
   console.log(`Congratz its ECMAScript!`)
 } else {
   console.log("You don't know? ECMAScript!")
+}
+
+ */
+
+let value = prompt("give me a number");
+
+if (value > 0) {
+  console.log("greater than zero")
+} else if (value < 0) {
+  console.log("less than zero")
+} else if (value == 0) {
+  console.log("equals zero")
+} else {
+  console.log("i need a number!")
 }
