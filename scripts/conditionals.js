19a3b502c6887bf7c2cc07f24b2d354a8d4ee27a
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-14 22:25:42 +0200

    Conditionals nr 5

--- scripts/conditionals.js
+++ scripts/conditionals.js
@@ -1,41 +1,7 @@
+let question = prompt("What is the 'official' name of JavaScript");
 
-const select = document.querySelector('select');
-// const para = document.querySelector('p');
-// beides ist möglich
-const para = document.getElementById('para');
-const html = document.querySelector('html');
-
-
-select.addEventListener('change', setWeather);
-
-function setWeather() {
-  const choice = select.value;
-  switch (choice) {
-    case 'sunny':
-      para.textContent = 'It is nice and sunny outside today';
-      break;
-    case 'rainy':
-      para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
-      break;
-    case 'snowing':
-      para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
-      break;
-    case 'overcast':
-      para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
-      break;
-    default:
-      para.textContent = '';
-  }
+if (question === "ECMAScript") {
+  console.log(`Congratz its ECMAScript!`)
+} else {
+  console.log("You don't know? ECMAScript!")
 }
-
-function update(bgColor, textColor) {
-  html.style.backgroundColor = bgColor;
-  html.style.color = textColor;
-}
-// Ternary operator
-
-select.onchange = function () {
-  (select.value === 'black') ? update('#333333', 'white') : update('lightgray', 'white');
-}
-
-let greeting = (isBirthday) ? 'Happy birthday Mrs. Smith — we hope you have a great day!' : 'Good morning Mrs. Smith.';
