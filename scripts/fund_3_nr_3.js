caa55a95b6203b4ddf7ab05885c217843adbcf19
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-27 20:56:29 +0200

    javascript basics with function

---
+++ scripts/fund_3_nr_3.js
@@ -0,0 +1,11 @@
+function favoriteAnimal(animal) {
+    console.log(animal + " is my favorite animal!")
+}
+favoriteAnimal("Goat")
+favoriteAnimal("Tiger")
+
+function showMessage() {
+    console.log('Hello everyone!');
+}
+showMessage()
+
