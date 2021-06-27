70b0a309fe3c1a3db9046a8d393d6fd331ecab42
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-27 21:17:12 +0200

    default value

--- scripts/fund_3_nr_3.js
+++ scripts/fund_3_nr_3.js
@@ -4,8 +4,9 @@ function favoriteAnimal(animal) {
 favoriteAnimal("Goat")
 
 
-function showMessage(from, text = "no text given") {
+function showMessage(from, text="default text") {
     console.log(from + ": " + text);
 }
-
-showMessage("Ann"); // Ann: no text given
+showMessage("Ann"); // Ann: default text
+showMessage(from = "tobi", text = "goes Hello") //tobi: goes Hello
+// Everything like in Python 3
