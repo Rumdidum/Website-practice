6d521fa1b7d05bdbf9a8eb9ac5829584b55bf557
Rumdidum <waldemar.rink2021@gmail.com>
2021-06-27 22:45:48 +0200

    rewriting functions

--- scripts/fund_3_nr_3.js
+++ scripts/fund_3_nr_3.js
@@ -1,4 +1,4 @@
-function favoriteAnimal(animal) {
+/* function favoriteAnimal(animal) {
     console.log(animal + " is my favorite animal!")
 }
 favoriteAnimal("Goat")
@@ -10,4 +10,26 @@ const n = 10;
 // looping from i = 1 to 5
 for (let i = 1; i <= n; i++) {
     console.log(`${i} I love JavaScript.`);
+} */
+
+ function min_if(a, b) {
+    if (a < b) {
+        return a
+    } else if (b < a) {
+        return b
+    }
 }
+
+console.log(min_if(1, 2))
+ 
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+
+console.log(min(1, -3))
+
+function min_or(a, b) {
+    return (a < b) || (b < a);
+}
+
+console.log(min_or(1, -3))
