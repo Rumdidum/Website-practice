5e6d93e9a5a508bf86b36467cee63622a6f4bf7f
Rumdidum <waldemar.rink2021@gmail.com>
2022-02-22 22:45:41 +0100

    Web dev simplified practice

--- scripts/scriptDOM.js
+++ scripts/scriptDOM.js
@@ -1,14 +1,26 @@
-/* function alertFunction() {
-    alert("YAY! YOU DID IT!");
-} */
-const btn = document.querySelector('#btn');
-/* // METHOD 2
-btn.onclick = alertFunction;
+const body = document.body
+const span_one = document.querySelector("#one")
 
+const container = document.createElement("div")
+container.id = "container"
 
-// METHOD 3
-btn.addEventListener('click', alertFunction); */
+const containerClass = container.classList.toggle("container-class")
+const div = document.createElement("div")
+div.id = "first-div"
+div.innerText = "hello"
 
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
-});
+const strong = document.createElement("strong")
+strong.innerText = "Strong Text?"
+
+const secondDiv = document.createElement("div")
+secondDiv.innerText = "second div"
+
+container.append(div)
+container.append(secondDiv)
+secondDiv.classList.toggle("two")
+
+div.append(strong)
+
+console.log(span_one.dataset)
+// div.innerHTML = "<strong>Hello world </strong>"
+body.append(container)
